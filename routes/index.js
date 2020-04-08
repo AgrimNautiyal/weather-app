8afa577cf012cbf49d8fd609c3ac76e132d98da9
unknown <agrimnautiyal1999@gmail.com>
2020-04-08 23:11:19 +0530

    Extended scope of results

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var router = require('express').Router();
 //var request = require('request');
-var express  = require('express');
 var request = require('request');
-var bodyParser = require('body-parser');
-var apiKey = 'cff7da7d2efa98aaf90c4d6be91a6153';
+require('dotenv').config();
+var apiKey = process.env.API_KEY;
 
 
 //router.use(bodyParser.json());
@@ -16,7 +15,6 @@ router.post('/displayDetails', function(req, res){
 
   //here we will define city and url
   var city = req.body.CityInput;
-  console.log(city);
   var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 request(url, function(err, response, body){
   if(err) {console.log('error : ', error);
@@ -30,15 +28,19 @@ res.render('errorpage');
       var weather = JSON.parse(body);
       //console.log(`The temperature is ${weather.main.temp} Kelvin`);
       console.log(weather);
-      if(response.statusCode==404){
+      if(response.statusCode==404){//this block evaluates conditions when city name doesn't exist in API's DB
         console.log('Not Found');
         res.render('errorpage');
 
       }
       else{
-      var temp = weather.main.temp;
+      var temp = weather.main.temp; //degree celsius
+      var humidity = weather.main.humidity; //percent
+      var pressure = weather.main.pressure; //hectopascals
+      var wind_speed = weather.wind.speed; //m/s
+      var wind_direction = weather.wind.deg;
 
-      res.render('result', {temp : temp});
+      res.render('result', {city:city, temp : temp, humidity:humidity, pressure:pressure, wind_speed:wind_speed, wind_direction:wind_direction});
 }
   }
 
